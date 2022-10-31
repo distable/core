da2f25ba9c1e507a2c81943df62743baf76456c5
oxysoft <teoxysoft@gmail.com>
2022-10-30 20:00:06 -0400

    Move classes to a separate lib

--- src_core/classes/prompt_job.py
+++
@@ -1,7 +0,0 @@
-from src_core.classes.JobParams import JobParams
-
-
-class prompt_job(JobParams):
-    def __init__(self, prompt: str = None, p: str = None, **kwargs):
-        super().__init__(**kwargs)
-        self.prompt = prompt or p or ''
